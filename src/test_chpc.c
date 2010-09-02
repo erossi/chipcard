7ee1eb0e3444d64d6a608078ddeb9e61b1b143ec
Enrico Rossi <e.rossi@tecnobrain.com>
2010-09-02 17:29:31 +0200

    Implemented CK and RST, can see ATR with scope on IO line

--- src/test_chpc.c
+++ src/test_chpc.c
@@ -15,13 +15,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define CHPC_PORT PORTC
-#define CHPC_PIN PINC
-#define CHPC_DDR DDRC
+#define CHPC_PORT PORTD
+#define CHPC_PIN PIND
+#define CHPC_DDR DDRD
 #define CHPC_PRESENT 0
 #define CHPC_RST 4
 #define CHPC_CK 6
 #define CHPC_IO 7
+#define OUT 1
+#define IN 0
+/* 10Khz = 100usec */
+#define CHPC_USEC 100
+#define ck_delay() _delay_us(100)
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -29,24 +34,111 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void)
+/* In or OUT */
+void set_io(const uint8_t io)
 {
-		/*
-		   PortC the chip card reader, PortD Leds
-		 */
+	if (io) {
+		CHPC_PORT &= ~(1<<CHPC_IO);
+		CHPC_DDR |= (1<<CHPC_IO);
+	} else {
+		CHPC_PORT |= (1<<CHPC_IO);
+		CHPC_DDR &= ~(1<<CHPC_IO);
+	}
+}
 
-		CHPC_DDR = 0;
-		CHPC_PORT = 0xff; /* PULL UP */
-		DDRD = 0xff; /* All OUT */
-		PORTD = 0xff; /* All leds off */
-
-		for (;;) {
-				loop_until_bit_is_set(CHPC_PIN, CHPC_PRESENT);
-				PORTD=0;
-				_delay_ms(1000);
-				loop_until_bit_is_clear(CHPC_PIN, CHPC_PRESENT);
-				PORTD=0xff;
-				_delay_ms(1000);
-		}
+/* 1 or 0 */
+void set_rst(const uint8_t rst)
+{
+	if (rst)
+		CHPC_PORT |= (1<<CHPC_RST);
+	else
+		CHPC_PORT &= ~(1<<CHPC_RST);
 
 }
+
+/* 1 or 0 */
+void set_ck(const uint8_t ck)
+{
+	if (ck)
+		CHPC_PORT |= (1<<CHPC_CK);
+	else
+		CHPC_PORT &= ~(1<<CHPC_CK);
+
+}
+
+uint8_t read_byte(void)
+{
+}
+
+void send_rst(void)
+{
+	uint8_t i;
+
+	set_io(IN);
+	set_rst(1);
+	set_ck(1);
+	ck_delay();
+	set_ck(0);
+	set_rst(0);
+	ck_delay();
+
+	for (i=0; i<32; i++) {
+		/* read bit in */
+		set_ck(1);
+		ck_delay();
+		set_ck(0);
+		ck_delay();
+	}
+
+}
+
+void init(void)
+{
+		/*
+		   Inital PORT setup:
+		   Card present 1 Pulled UP by micro
+		   Reset 0
+		   Clock 0
+		   IO 1 Pulled UP when Reading
+		   */
+		
+		CHPC_PORT = (1 << CHPC_PRESENT) | (1 << CHPC_IO);
+
+		/*
+		   Initial DDR setup:
+		   Card Present - IN
+		   Reset - OUT
+		   Clock - OUT
+		   IO - BiDirectional begin IN
+		   */
+
+		CHPC_DDR = (1 << CHPC_RST) | (1 << CHPC_CK);
+}
+
+int main(void)
+{
+	*char atr;
+
+	init();
+
+	DDRC = 1; /* PC0 OUT */
+	PORTC = 1; /* PC0 led off */
+
+	atr = malloc(4);
+
+	for (;;) {
+		loop_until_bit_is_set(CHPC_PIN, CHPC_PRESENT);
+		PORTC=0;
+		_delay_ms(1000);
+
+		send_rst(atr);
+
+		loop_until_bit_is_clear(CHPC_PIN, CHPC_PRESENT);
+		PORTC=1;
+		_delay_ms(1000);
+	}
+
+	free(atr);
+}
+
+/* vim: set nu cindent ts=4 sw=4: */
